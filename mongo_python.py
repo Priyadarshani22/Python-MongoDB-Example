a0c3a65058a06d0fab6f59d87d8138d6487a7b3a
Priyadarshani22 <96864642+Priyadarshani22@users.noreply.github.com>
2021-12-30 14:21:59 +0530

    Update mongo_python.py

--- mongo_python.py
+++ mongo_python.py
@@ -1,51 +1,8 @@
 """
 Name: MongoDB Python Demo app
-Author :  Rajiv Sharma
+Author :  priyadarshani pisal
 Developer Website : www.hqvfx.com
-Developer Email   : rajiv@hqvfx.com
-Date Started : 12 december 2018
-Date Modified :
-Description : Create tableView and create a custom model to populate data
-
-Download Application from : www.hqvfx.com/downloads
-Source Code Website : www.github.com/hqvfx
-Free Video Tutorials : www.youtube.com/vfxpipeline
-
-Copyright (c) 2018, HQVFX(www.hqvfx.com) . All rights reserved.
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-
-    * Neither the name of HQVFX(www.hqvfx.com) nor the names of any
-      other contributors to this software may be used to endorse or
-      promote products derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
-
-from PySide2 import QtWidgets
-from PySide2 import QtGui
-from PySide2 import QtCore
-from gui import main
-from lib import databaseOperations
-from lib import customModel
+Developer Email   : pisal152001@gmail.com
 
 
 class PythonMongoDB(main.Ui_MainWindow, QtWidgets.QMainWindow):
@@ -53,7 +10,7 @@ class PythonMongoDB(main.Ui_MainWindow, QtWidgets.QMainWindow):
         super(PythonMongoDB, self).__init__()
         self.setupUi(self)
 
-        # data = {'photo': ":/icons/images/photo.jpg",  'name': 'rajiv'}
+        # data = {'photo': ":/icons/images/photo.jpg",  'name': 'priyadarshani'}
         # databaseOperations.insert_data(data)
 
         self.user_data = databaseOperations.get_multiple_data()
